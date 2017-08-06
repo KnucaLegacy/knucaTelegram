f41d5c2c2a4d509b2a5883f20ee849c738252ca7
theopus <theopus5@gmail.com>
2017-08-06 13:37:57 +0300

    start message fix

--- src/main/java/com/theopus/knucaTelegram/KnucaTelegramApplication.java
+++ src/main/java/com/theopus/knucaTelegram/KnucaTelegramApplication.java
@@ -2,6 +2,8 @@ package com.theopus.knucaTelegram;
 
 import com.theopus.knucaTelegram.parser.core.MainParser;
 import com.theopus.knucaTelegram.service.data.LessonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,9 +14,12 @@ import java.io.IOException;
 //@ComponentScan(value = "com.theopus.knucaTelegram")
 public class KnucaTelegramApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(KnucaTelegramApplication.class);
+
 
     public static void main(String[] args) throws IOException {
         ConfigurableApplicationContext run = SpringApplication.run(KnucaTelegramApplication.class, args);
+        log.info("in main starting");
 //        run.getBean(LessonService.class).saveAll(MainParser.parseFolder("pdfs"));
     }
 }
