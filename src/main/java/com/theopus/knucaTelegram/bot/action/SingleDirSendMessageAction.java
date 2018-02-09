c081a19f54eb5e61b72982a2611ec298b2ab2a35
Oleksandr Tkachov <Oleksandr_Tkachov@epam.com>
2018-02-09 15:25:07 +0200

    Ads message update.

--- src/main/java/com/theopus/knucaTelegram/bot/action/SingleDirSendMessageAction.java
+++ src/main/java/com/theopus/knucaTelegram/bot/action/SingleDirSendMessageAction.java
@@ -2,11 +2,14 @@ package com.theopus.knucaTelegram.bot.action;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.theopus.knucaTelegram.bot.util.TelegramMessageFormater;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.bots.AbsSender;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.util.Collection;
+import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class SingleDirSendMessageAction implements SendDataAction {
 
@@ -17,6 +20,7 @@ public abstract class SingleDirSendMessageAction implements SendDataAction {
     @JsonIgnore
     protected TelegramMessageFormater formater = new TelegramMessageFormater();
 
+    private static final Logger LOG = LoggerFactory.getLogger(SingleDirSendMessageAction.class);
 
     public SingleDirSendMessageAction(long chatId) {
         this.chatId = chatId;
@@ -26,8 +30,20 @@ public abstract class SingleDirSendMessageAction implements SendDataAction {
     public abstract Collection<SendMessage> buildMessage();
 
     @Override
-    public void execute(AbsSender bot){
+    public void execute(AbsSender bot) {
         this.messages = buildMessage();
+        int l =ThreadLocalRandom.current().nextInt(100);
+
+        LOG.info("Add roll = {}", l);
+        if (l < 70) {
+            messages.add(
+                    new SendMessage(
+                            0L,
+                            "\n Предложения, вопросы? [Официальная группа бота в телеграме.](https://t.me/joinchat/Dlb5FQ7SKH1iEICDdpDN7Q)"
+                    )
+                            .enableMarkdown(true)
+            );
+        }
         messages.forEach(message -> {
             message.setChatId(this.chatId);
             try {
